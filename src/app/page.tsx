54a882534939f24a09fe97a67fab572ef125c57c
minhoshin11 <tlsalsgh777@naver.com>
2024-06-24 16:56:35 +0900

    몽고디비 타입 배포오류로 인한 재시도

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import { connectDB } from "../utils/database";
+import { connectDB } from "@/utils/database";
 
-export default async function Home() {
-  const client = await connectDB;
-  const db = client.db("forum");
-  let result = await db.collection("post").find().toArray();
-  console.log(result, "폴더구조 잡기1");
+export default function Home() {
+  async function fetchData() {
+    try {
+      const client = await connectDB;
+      const db = client.db("forum");
+      const result = await db.collection("post").find().toArray();
+      console.log(result, "폴더구조 잡기1");
+      // 여기서 result를 상태로 저장하거나 JSX로 렌더링합니다.
+    } catch (error) {
+      console.error("데이터베이스 연결 오류", error);
+      // 데이터베이스 연결 오류 발생 시 처리 로직을 작성합니다.
+    }
+  }
+
+  // 컴포넌트가 마운트될 때 데이터를 가져오기 위해 fetchData 함수 호출
+  fetchData();
+
   return <div>폴더구조 만들기</div>;
 }
--- src/utils/database.tsx
+++ src/utils/database.tsx
@@ -7,9 +7,11 @@ const options = {}; // useNewUrlParser 생략
 let connectDB: Promise<MongoClient>;
 
 declare global {
+  // 전역 변수를 확장하여 _mongo를 추가
+  // _mongo는 Promise<MongoClient> 타입
   namespace NodeJS {
     interface Global {
-      _mongo?: Promise<MongoClient>;
+      _mongo: Promise<MongoClient>;
     }
   }
 }
@@ -17,10 +19,10 @@ declare global {
 // 개발 환경인지 확인
 if (process.env.NODE_ENV === "development") {
   // 개발 환경에서 _mongo가 없는 경우 새로 연결
-  if (!(global as any)._mongo) {
-    (global as any)._mongo = new MongoClient(url, options).connect();
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect();
   }
-  connectDB = (global as any)._mongo;
+  connectDB = global._mongo;
 } else {
   // 배포 환경에서는 새로 연결
   connectDB = new MongoClient(url, options).connect();
